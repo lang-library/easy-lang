d8724780a21b06c4bb3a02bade885be0731fe84a
JavaCommons Technologies <javacommons@gmail.com>
2024-07-02 18:01:29 +0900

    .

--- el/easy-lang/ei.el
+++ el/easy-lang/ei.el
@@ -1,17 +1,16 @@
 (load "~/.emacs.d/xprint.el")
 (require 'eieio)
-
 (defclass <rectangle> ()
   ((width  :initarg :width)
    (height :initarg :height))
   )
-
 (cl-defmethod !area ((this <rectangle>))
   (with-slots
       (width height)
       this
     (* width height)))
-
 (setq rect (make-instance <rectangle> :width 20 :height 10))
-
+(xdump (slot-value rect 'width))
+(xdump (slot-value rect 'height))
 (xdump (!area rect))
+(xdump [a b c])
