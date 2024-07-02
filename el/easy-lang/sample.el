745a8e57420e686f06cd5d8596597af892f0286a
JavaCommons Technologies <javacommons@gmail.com>
2024-07-03 08:50:49 +0900

    .

---
+++ el/easy-lang/sample.el
@@ -0,0 +1,19 @@
+(load "~/.emacs.d/xprint.el")
+(require 'eieio)
+(!class <rectangle> ()
+  ((:width  1.0)
+   (:height 1.0)
+   :temp)
+  )
+(!method !area ((this <rectangle>))
+  (with-slots
+      (width height)
+      this
+    (return (* width height))))
+(setq rect (!new <rectangle> :width 20 :height 10))
+(xdump [! rect :width])
+(xdump [! rect :height])
+(xdump [! rect !area])
+
+[! ary [0]]
+[! ht [:key]]
