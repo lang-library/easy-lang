ab452e1900a157e43219a5adbd54a61787986468
JavaCommons Technologies <javacommons@gmail.com>
2024-07-04 20:45:43 +0900

    .

--- el/easy-lang/run.sh
+++ el/easy-lang/run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 set -uvx
 set -e
-#emacs -batch -l easy-lang.el a b "c d"
-emacs -batch -l boot.el -l easy-lang.el -l test.el -f test-func a b "c d"
+emacs -batch -l easy-lang.el a b "c d"
+#emacs -batch -l boot.el -l easy-lang.el a b "c d"
